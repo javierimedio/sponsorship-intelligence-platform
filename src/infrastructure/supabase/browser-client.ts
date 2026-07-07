ea63e33a4becbaace07131479e3ef008efd158c5
javierimedio <javierimedio@gorfactory.es>
2026-07-07 13:58:31 +0200

    Create browser-client.ts

---
+++ src/infrastructure/supabase/browser-client.ts
@@ -0,0 +1,9 @@
+// src/infrastructure/supabase/browser-client.ts
+import { createBrowserClient } from '@supabase/ssr';
+
+export function createSupabaseBrowserClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  );
+}
