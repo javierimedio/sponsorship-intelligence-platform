6a3843c0e6d4f0c641cb9bfd32efb9a00c277296
javierimedio <javierimedio@gorfactory.es>
2026-07-07 14:21:29 +0200

    Update ids.ts

--- src/domain/shared/ids.ts
+++ src/domain/shared/ids.ts
@@ -6,8 +6,12 @@ export type TenantId = string & { readonly __brand: 'TenantId' };
 export type OrganizationId = string & { readonly __brand: 'OrganizationId' };
 export type BrandId = string & { readonly __brand: 'BrandId' };
 export type UserId = string & { readonly __brand: 'UserId' };
+export type ProposalId = string & { readonly __brand: 'ProposalId' };
+export type DocumentId = string & { readonly __brand: 'DocumentId' };
 
 export const asTenantId = (id: string): TenantId => id as TenantId;
 export const asOrganizationId = (id: string): OrganizationId => id as OrganizationId;
 export const asBrandId = (id: string): BrandId => id as BrandId;
 export const asUserId = (id: string): UserId => id as UserId;
+export const asProposalId = (id: string): ProposalId => id as ProposalId;
+export const asDocumentId = (id: string): DocumentId => id as DocumentId;
